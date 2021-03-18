dce94534f68178419196499b01ef44a6efa94895
softwarece1 <softwarece1@bel.co.in>
2021-03-18 11:48:04 +0530

    first commit

---
+++ src/test/java/com/bel/eurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bel.eurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@SpringBootApplication
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
